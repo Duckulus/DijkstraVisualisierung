8a27432b8fcb716c41e96090da6e3d8d735aa073
Duckulus <duckelite1@gmail.com>
2022-02-15 18:08:13 +0100

    Endlosschleife behoben und Kommentare hinzugefügt

--- src/main/java/de/amin/dijkstrafacharbeit/data/Dijkstra.java
+++ src/main/java/de/amin/dijkstrafacharbeit/data/Dijkstra.java
@@ -3,143 +3,166 @@ package de.amin.dijkstrafacharbeit.data;
 import java.util.HashMap;
 import java.util.Map;
 
-class Dijkstra {
-
+/**
+ * Diese Klasse repräsentiert den Dijkstra algorithmus
+ *
+ * @author Amin Haddou
+ */
+public class Dijkstra {
     /**
-     * Der zu bearbeitende Graph
+     * Zuordnung von Knoten zu ihrer gesamten Diszanz zum Start
      */
-    private final Graph graph;
+    private final Map<Vertex, Double> distance;
     /**
-     * Der Ausgangsknoten der Wegfindung
+     * Zuordnung von Knoten zu ihren Vorgängern
      */
-    private final Vertex start;
-    /**
-     * Der Zielknoten der Wegfindung
-     */
-    private final Vertex goal;
+    private final Map<Vertex, Vertex> previous;
     /**
      * Liste aller unbesuchten Knoten
      */
     private final List<Vertex> unvisited;
-    /**
-     * Zuordnung aller Knoten und ihrer Distanz zum Ursprung
-     */
-    private final Map<Vertex, Double> distance;
-    /**
-     * Zuordnung aller Knoten zu ihrem vorgänger
-     */
-    private final Map<Vertex, Vertex> previous;
 
     /**
-     * Initialisiert ein Dijkstra Objekt, welches der Findung des kürzesten
-     * Pfads zwischen zwei Knoten auf einem Graphen dient
-     * @param graph Der zu bearbeitende Graph
-     * @param start Der Startknoten
-     * @param goal Der Zielknoten
+     * Instanziiert ein neues {@link Dijkstra} Objekt
      */
-    public Dijkstra(Graph graph, Vertex start, Vertex goal) {
-        this.graph = graph;
-        this.start = start;
-        this.goal = goal;
-        unvisited = new List<>();
+    public Dijkstra() {
         distance = new HashMap<>();
         previous = new HashMap<>();
+        unvisited = new List<>();
+    }
 
+    /**
+     * Berechnet den kürzesten Weg zwischen 2 Knoten auf einem Graphen
+     * @param graph Der zu bearbeitende Graph
+     * @param start Der Startpunkt
+     * @param goal Der Zielpunkt
+     * @return Eine Liste, welche alle zu übergehenden Knoten auf dem Weg vom Start zum Ziel in geordneter Reihenfolge enthält
+     */
+    public List<Vertex> shortestPath(Graph graph, Vertex start, Vertex goal) {
+        dijkstra(graph, start);
+        List<Vertex> path = new List<>();
+
+        Vertex temp = goal;
+
+        path.append(temp);
+
+        while (previous.get(temp) != null) {
+            temp = previous.get(temp);
+            path.toFirst();
+            path.insert(temp);
+        }
+
+        return path;
+    }
+
+    /**
+     * Berechnet die Distanz zu allen Punkten eines Graphs von einem bestimmen Startpunkt
+     * @param graph Der betroffene Graph
+     * @param start Der Startpunkt
+     */
+    private void dijkstra(Graph graph, Vertex start) {
+        initialize(graph, start);
+        while (!unvisited.isEmpty()) { //Abbruchbedingung: Alle felder wurden besucht
+            Vertex u = getSmallest(); //Kleinster unbesuchter Knoten als ausgangspunkt
+
+            visit(u); //Besuchter knoten wird als besucht markiert
+
+            List<Vertex> neighbours = getNeighbours(graph, u);
+            neighbours.toFirst();
+            while (neighbours.hasAccess()) { // Die Distanz zu allen Nachbarn des aktuellen Knoten wird neu berechnet
+                if (!isVisited(neighbours.getContent())) {
+                    updateDistance(graph, u, neighbours.getContent());
+                }
+                neighbours.next();
+            }
+        }
+    }
+
+    /**
+     *  Setzt die Abstände von allen Knoten, bis auf den Startpunkt, auf Unendlich
+     *  und markiert alle Knoten als unbesucht. Dies ist die Grundlage, damit der
+     *  Dijkstra Algorithmus operieren kann und muss daher davor ausgeführt werden
+     * @param graph Der betroffene Graph
+     * @param start Der Startpunkt
+     */
+    private void initialize(Graph graph, Vertex start) {
+        List<Vertex> vertices = graph.getVertices();
+
+        vertices.toFirst();
+        while (vertices.hasAccess()) {
+            distance.put(vertices.getContent(), Double.MAX_VALUE); //initialisierung aller Knoten mit unendlichem Abstand (Double Limit)
+            previous.put(vertices.getContent(), null);
+
+
+            unvisited.append(vertices.getContent());
+
+            vertices.next();
+        }
+        distance.put(start, 0.0);
+
+    }
+
+    /**
+     * Gibt den unbesuchten Punkt mit der niedrigsten Distanz zurück
+     * @return Unbesuchter Punkt mit niedrigster Distanz
+     */
+    private Vertex getSmallest() {
+        Vertex smallest = null;
+        double value = Double.MAX_VALUE;
+
+        unvisited.toFirst();
+        while (unvisited.hasAccess()) {
+            if (distance.get(unvisited.getContent()) < value) {
+                value = distance.get(unvisited.getContent());
+                smallest = unvisited.getContent();
+            }
+            unvisited.next();
+        }
+        return smallest;
+    }
+
+    /**
+     * Entfernt einen Knoten aus der Liste der unbesuchten Knoten
+     * @param vertex Der betroffene Knoten
+     */
+    private void visit(Vertex vertex) {
+        unvisited.toFirst();
+        while (unvisited.hasAccess()) {
+            if (unvisited.getContent() == vertex) {
+                unvisited.remove();
+            }
+            unvisited.next();
+        }
+    }
+
+    /**
+     * Gibt die benachbarten Knoten eines Knotens in einem Graphen zurück
+     * @param graph Der betroffene Graph
+     * @param vertex Der betroffene Knoten
+     * @return Liste mit allen verbundenen Knoten
+     */
+    private List<Vertex> getNeighbours(Graph graph, Vertex vertex) {
+        List<Vertex> neighbours = new List<>();
         List<Vertex> vertices = graph.getVertices();
         vertices.toFirst();
         while (vertices.hasAccess()) {
-            unvisited.append(vertices.getContent()); //Markieren aller Knoten als unbesucht
-            distance.put(vertices.getContent(), Double.MAX_VALUE); //Zuweisung der Distanz aller Knoten als Unendlich(Maximaler Integer Wert)
+            if (graph.getEdge(vertex, vertices.getContent()) != null) {
+                neighbours.append(vertices.getContent());
+            }
             vertices.next();
         }
-
-        distance.put(start, 0D); //Startpunkt wird die Distanz 0 zugewiesen
-        unvisited.toFirst();
-        while (unvisited.hasAccess()) {
-            if (unvisited.getContent() == start) {
-                unvisited.remove(); //Entfernen des Startpunkts aus der Liste der Unbesuchten Knoten
-            }
-            unvisited.next();
-        }
-    }
-
-    public String kuerzesterWeg() {
-        Vertex k = start;
-        while (!goalReached()) {
-            List<Vertex> vertices = graph.getVertices();
-            vertices.toFirst();
-            while (vertices.hasAccess()) {
-                if(graph.getEdge(vertices.getContent(), k)!=null) {
-                    distance.put(vertices.getContent(),distance.get(k) + graph.getEdge(vertices.getContent(),k).getWeight());
-                }
-                vertices.next();
-            }
-
-            Vertex temp = k;
-            k = knotenMitKleinsterDistanz(k);
-            previous.put(k, temp);
-
-            unvisited.toFirst();
-            while (unvisited.hasAccess()) {
-                if(unvisited.getContent()==k) {
-                    unvisited.remove();
-                }
-                unvisited.next();
-            }
-
-            if(goalReached()) {
-                Vertex current = k;
-                String pathString = "";
-                List<Vertex> vertexPath = new List<>();
-                vertexPath.append(current);
-                while (previous.get(current)!=null){
-                    vertexPath.append(previous.get(current));
-                    current = previous.get(current);
-                }
-                vertexPath = reverse(vertexPath);
-                vertexPath.toFirst();
-
-                vertexPath.toFirst();
-                while (vertexPath.hasAccess()) {
-                    pathString+=vertexPath.getContent().getID();
-                    vertexPath.next();
-                    if(vertexPath.hasAccess()) {
-                        pathString+= "  ->  ";
-                    }
-                }
-                pathString+="  Total Cost: " + distance.get(goal);
-                return pathString;
-            }
-        }
-        return "No Path Found";
+        return neighbours;
     }
 
     /**
-     * Diese Methode gibt den Knoten mit der kleinsten direkten Distanz zum Ursprungsknoten zurück
-     * @param origin Der Ursprungsknoten
-     * @return Den Knoten mit der kürzesten direkten Distanz zu origin, oder null, falls keine Direkte Verbindung besteht.
+     * Gibt zurück ob ein Knoten bereits besucht wurde
+     * @param vertex Der betroffene Knoten
+     * @return True wenn der Knoten besucht wurde, anderfalls False
      */
-    private Vertex knotenMitKleinsterDistanz(Vertex origin) {
-        Vertex k = null;
-
-        double value = Double.MAX_VALUE; //Zwischenspeicher um aktuell kleinsten wert als Vergleich zu speichern
-        unvisited.toFirst();
-        while (unvisited.hasAccess()) {
-            Edge edge = graph.getEdge(origin, unvisited.getContent());
-            if (edge != null && edge.getWeight() < value) {
-                value = edge.getWeight();
-                k = unvisited.getContent();
-            }
-            unvisited.next();
-        }
-
-        return k;
-    }
-
     private boolean isVisited(Vertex vertex) {
         unvisited.toFirst();
         while (unvisited.hasAccess()) {
-            if (unvisited.getContent()==vertex) {
+            if (unvisited.getContent() == vertex) {
                 return false;
             }
             unvisited.next();
@@ -148,36 +171,18 @@ class Dijkstra {
     }
 
     /**
-     * Diese Methode überprüft ob das Ziel erreicht ist,
-     * indem sie schaut ob der Zielknoten in der Liste der unbesuchten
-     * Knoten ist
-     * @return True wenn das Ziel erreicht ist
+     * Berechnet die Distanz eines Knotens neu basierend auf der Distanz einer seiner Nachbarn
+     * und setzt den Nachbarn als Vorgänger
+     * @param graph Der betroffene Graph
+     * @param u Der benachbarte Knoten
+     * @param v Der Knoten der aktualisiert wird
      */
-    private boolean goalReached() {
-        boolean reached = true;
-
-        unvisited.toFirst();
-        while (unvisited.hasAccess()) {
-            if(unvisited.getContent()==goal) {
-                reached=false;
-            }
-            unvisited.next();
+    private void updateDistance(Graph graph, Vertex u, Vertex v) {
+        double alternative = distance.get(u) + graph.getEdge(u, v).getWeight();
+        if (alternative < distance.get(v)) {
+            distance.put(v, alternative);
+            previous.put(v, u);
         }
-
-        return reached;
-    }
-
-
-    public<T> List<T> reverse(List<T> toReverse) {
-        List<T> reversed = new List<>();
-        toReverse.toLast();
-        while (toReverse.hasAccess()) {
-            reversed.append(toReverse.getContent());
-            toReverse.remove();
-            toReverse.toLast();
-        }
-
-        return reversed;
     }
 
 }
